1aa54f6d7398371b5dd7aaf461508813f7cb7ec2
JaimeVilches87 <jaimevilches87@gmail.com>
2021-12-22 23:02:24 -0500

    added home profile and background

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Typical from "react-typical";
+import './Profile.css';
 
 export default function Profile() {
   return (
@@ -6,22 +8,69 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="#">
-              <i className=" fa fa-facebook-square"></i>
-            </a>
-            <a href="#">
-              <i className=" fa fa-instagram"></i>
-            </a>
-            <a href="#">
-              <i className=" fa fa-youtube-square"></i>
-            </a>
-            <a href="#">
-              <i className=" fa fa-google-plus-square"></i>
-            </a>
-            <a href="#">
-              <i className=" fa fa-twitter"></i>
+            <div className="colz-icon">
+              <a href="#">
+                <i className=" fa fa-facebook-square"></i>
+              </a>
+              <a href="#">
+                <i className=" fa fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i className=" fa fa-youtube-square"></i>
+              </a>
+              <a href="#">
+                <i className=" fa fa-google-plus-square"></i>
+              </a>
+              <a href="#">
+                <i className=" fa fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {""}
+              Hello, I'M <span className="highlighted-text">Jaime</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {""}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Result Oriented Developer",
+                    1000,
+                    "Full Stack Developer",
+                    1000,
+                    "HTML, CSS & JavaScript Dev",
+                    1000,
+                    "Cross Platform v",
+                    1000,
+                    "React/React Native Dev",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building applications with front and back end
+                operations.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me {""}
+            </button>
+            <a href="JaimeResume.pdf" download="Jaime JaimeResume.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
             </a>
           </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
